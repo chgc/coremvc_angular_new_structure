4fb449ebe3eada6e5f6e8076bdcf32203f5d0bd4
Kevin Yang <chgc_kevin@hotmail.com>
2017-09-11 16:37:47 +0800

    minor fix

--- README.md
+++ README.md
@@ -2,20 +2,20 @@
 Angular與 Core MVC 新的纏綿方式
 
 
-整合 Angular 與 ASP.NET MVC Core 在一起
+整合 Angular 與 ASP.NET MVC Core 在一起  + SSR 功能 
 
 # .angular-cli.json
 
 修改 outDir 到 mvc 專案下的 wwwroot/dist 資料夾
 
-設定 deployUrl 為 "/dist/"，只要目的是要確保輸出的 script 檔案路徑是相對於 wwwroot 的 dist 路徑
+設定 deployUrl 為  `/dist/`，只要目的是要確保輸出的 script 檔案路徑是相對於 wwwroot 的 dist 路徑
 
-修改 index 為 "<any>.html" 檔案，這個檔案只負責儲存 angular-cli 建置後的 script 檔案名稱及路徑
+修改 index 為 `script.html` 檔案，這個檔案只負責儲存 angular-cli 建置後的 script 檔案名稱及路徑
 
 
 # _layout.cshtml
 
-將 "<any>.html" 檔案加入
+將 `script.html` 檔案加入
 
 ```
 @Html.Partial("~/wwwroot/dist/script.html")
@@ -27,7 +27,8 @@ Angular與 Core MVC 新的纏綿方式
 **HomeController 新增 `Angular` Action function**
 
 ```
-routes.MapRoute("spa-fallback", "{*anything}", new { controller = "Home", action = "Angular" });
+ routes.MapSpaFallbackRoute(name: "spa-fallback",
+                            defaults: new { controller = "Home", action = "Angular" });
 ```
 
 依此規則，在 Views/Home/Angular.cshtml 裡，新增 `<app-root></app-root>`
--- WebApplication2/Controllers/HomeController.cs
+++ WebApplication2/Controllers/HomeController.cs
@@ -35,17 +35,19 @@ namespace WebApplication2.Controllers
 
         public async Task<IActionResult> Angular()
         {
-            var bundlePath = $"{_hostingEnvironment.ContentRootPath}/angularApp/dist-server/main.bundle";
-            RenderToStringResult prerenderResult = await new AngularUniversal(Request).Render(bundlePath);
+            if (_hostingEnvironment.EnvironmentName == "Proudction")
+            {
+                var bundlePath = $"{_hostingEnvironment.ContentRootPath}/angularApp/dist-server/main.bundle";
+                RenderToStringResult prerenderResult = await new AngularUniversal(Request).Render(bundlePath);
 
-            ViewData["SpaHtml"] = prerenderResult.Html; // our <app> from Angular
-            ViewData["Title"] = prerenderResult.Globals["title"]; // set our <title> from Angular
-            ViewData["Styles"] = prerenderResult.Globals["styles"]; // put styles in the correct place
-            ViewData["Scripts"] = prerenderResult.Globals["scripts"]; // scripts (that were in our header)
-            ViewData["Meta"] = prerenderResult.Globals["meta"]; // set our <meta> SEO tags
-            ViewData["Links"] = prerenderResult.Globals["links"]; // set our <link rel="canonical"> etc SEO tags
-            ViewData["TransferData"] = prerenderResult.Globals["transferData"]; // our transfer data set to window.TRANSFER_CACHE = {};
-
+                ViewData["SpaHtml"] = prerenderResult.Html; // our <app> from Angular
+                ViewData["Title"] = prerenderResult.Globals["title"]; // set our <title> from Angular
+                ViewData["Styles"] = prerenderResult.Globals["styles"]; // put styles in the correct place
+                ViewData["Scripts"] = prerenderResult.Globals["scripts"]; // scripts (that were in our header)
+                ViewData["Meta"] = prerenderResult.Globals["meta"]; // set our <meta> SEO tags
+                ViewData["Links"] = prerenderResult.Globals["links"]; // set our <link rel="canonical"> etc SEO tags
+                ViewData["TransferData"] = prerenderResult.Globals["transferData"]; // our transfer data set to window.TRANSFER_CACHE = {};
+            }
             return View();
         }
 
